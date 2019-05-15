890bf25399eeefe18011b88eee269660a55c56d6
Rika Lena Denia <rika.denia@comnetgmbh.com>
2019-05-15 15:52:17 +0000

    sakuli-legacy: Move capability creation to capabilities-builder.class.ts

--- packages/sakuli-legacy/src/context/legacy-Lifecycle-hooks.class.ts
+++ packages/sakuli-legacy/src/context/legacy-Lifecycle-hooks.class.ts
@@ -11,19 +11,20 @@ import {createLoggerClass} from "./common/logger.class";
 import {LegacyProjectProperties} from "../loader/legacy-project-properties.class";
 import {createRegionClass} from "./common/sakuli-region.class";
 import {createApplicationClass} from "./common/sakuli-application.class";
+import {CapabilitiesBuilderInterface, GenericCapabilitiesBuilder} from "../loader/capabilities-builder.class";
 
 export class LegacyLifecycleHooks implements TestExecutionLifecycleHooks {
 
 
-    capabilityMap: { [key: string]: () => Capabilities } = {
-        'chrome': () => Capabilities.chrome(),
-        'firefox': () => Capabilities.firefox(),
-        'edge': () => Capabilities.edge(),
-        'safari': () => Capabilities.safari(),
-        'ie': () => Capabilities.ie(),
-        'phantomjs': () => Capabilities.phantomjs(),
-        'htmlunit': () => Capabilities.htmlunit(),
-        'htmlunitwithjs': () => Capabilities.htmlunitwithjs(),
+    capabilityMap: { [key: string]: (project : Project) => CapabilitiesBuilderInterface } = {
+        'chrome': (project) => new GenericCapabilitiesBuilder(project),
+        'firefox': (project) => new GenericCapabilitiesBuilder(project),
+        'edge': (project) => new GenericCapabilitiesBuilder(project),
+        'safari': (project) => new GenericCapabilitiesBuilder(project),
+        'ie': (project) => new GenericCapabilitiesBuilder(project),
+        'phantomjs': (project) => new GenericCapabilitiesBuilder(project),
+        'htmlunit': (project) => new GenericCapabilitiesBuilder(project),
+        'htmlunitwithjs': (project) => new GenericCapabilitiesBuilder(project),
     };
     driver: Maybe<ThenableWebDriver> = null;
 
@@ -37,11 +38,12 @@ export class LegacyLifecycleHooks implements TestExecutionLifecycleHooks {
         //const props: LegacyProjectProperties = project.
         const properties = project.objectFactory(LegacyProjectProperties);
         const browser: keyof typeof Capabilities = properties.testsuiteBrowser;
-        const capsProducer = throwIfAbsent(this.capabilityMap[browser], Error(`${browser} is not a valid browser`));
-        const caps = capsProducer();
+        const capsBuilderProducer = throwIfAbsent(this.capabilityMap[browser], Error(`${browser} is not a valid browser`));
+        const capsBuilder = capsBuilderProducer(project);
+
         this.driver = this.builder
             .forBrowser(browser)
-            .withCapabilities(caps)
+            .withCapabilities(capsBuilder.build())
             .build();
     }
 
